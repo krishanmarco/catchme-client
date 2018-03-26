de52690863b2835cfec19a4d2206d52736dad7c0
Krishan Marco Madan <krishanmarco@outlook.com>
2018-03-26 23:23:07 +0200

    Implemented Button loader

--- app/comp/misc/GradientButton.js
+++ app/comp/misc/GradientButton.js
@@ -32,7 +32,7 @@ const DefaultProps = {
 // Const *************************************************************************************************
 // Const *************************************************************************************************
 
-export default class GradientButton extends RkComponent<DefaultProps, Props> {
+export default class GradientButton extends RkComponent<any, Props> {
 	static defaultProps = DefaultProps;
 
 	// Needed for this.defineStyles()
